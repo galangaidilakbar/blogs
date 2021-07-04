e2a13536e1f288088d2551e8bce2f441e88e913f
Galangaidil <galangaidil45@gmail.com>
2021-07-04 16:48:04 +0700

    coba tangkap data yang dikirimkan view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,5 +8,7 @@
 </head>
 <body>
   <h1>Halaman About</h1>
+  <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+  <p>Halo nama saya {{ $name }}, email saya {{ $email }}</p>
 </body>
 </html>
