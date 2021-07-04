1946c696f24abe7d4fce59abf84f2d747c4dd409
Galangaidil <galangaidil45@gmail.com>
2021-07-04 16:20:51 +0700

    nambahin route home, about, blog

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
