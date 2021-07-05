b901c73fdf46d28e30b117c52a3c404dfd3ce1ad
Galangaidil <galangaidil45@gmail.com>
2021-07-06 00:55:56 +0700

    membuat slug manual

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,9 @@
 @section('container')
     @foreach ($posts as $post)
     <article class="mb-5">
-      <h2>{{ $post['title'] }}</h2>
+      <h2>
+        <a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a>
+      </h2>
       <h5>By: {{ $post['author'] }}</h5>
       <p>{{ $post['body'] }}</p>
     </article>
