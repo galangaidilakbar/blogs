939b6741ae6661deab6872a55721b3d91d3172a2
Galangaidil <galangaidil45@gmail.com>
2021-07-06 00:51:38 +0700

    simulasi blog post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.main')
 @section('container')
-    <h1>Halaman Posts</h1>
+    @foreach ($posts as $post)
+    <article class="mb-5">
+      <h2>{{ $post['title'] }}</h2>
+      <h5>By: {{ $post['author'] }}</h5>
+      <p>{{ $post['body'] }}</p>
+    </article>
+    @endforeach
 @endsection
+
--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,23 @@ Route::get('/about', function () {
     ]);
 });
 
+
 Route::get('/blog', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "author" => "Galang aidil akbar",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus, iusto alias ad laborum dolorum deleniti aspernatur, quae temporibus est ea nihil ratione assumenda aliquam earum unde accusamus doloribus itaque quas. Magnam, magni voluptatum adipisci quod asperiores sint pariatur eligendi officia suscipit optio aperiam, aliquam porro cupiditate consequatur exercitationem ad dolorum laudantium aspernatur repudiandae officiis. Quos earum amet atque, a repellat voluptate necessitatibus eius pariatur ipsum harum molestias aut? Fugiat fuga, nam accusamus qui placeat facilis impedit enim pariatur ut hic!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "author" => "Nela Puspita Sari",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi maiores labore neque. Corporis a consequuntur placeat ipsa modi porro excepturi odit rerum, iure praesentium sequi ex officia facere suscipit eum laborum numquam quaerat officiis error velit! Nesciunt autem dignissimos quis cum, error aliquam dicta delectus doloremque in, vel facilis, tenetur molestias tempore ullam aliquid officia exercitationem eos modi perspiciatis harum! Voluptas dolore mollitia cupiditate dolores rerum eos nemo corrupti, perferendis, maiores expedita repudiandae. Nulla nihil dignissimos illum minima similique ipsum aliquid iste dolorem. Eaque accusamus ipsum odio, officia alias libero ullam laudantium eos eveniet voluptate consequatur repudiandae optio amet. Porro?"
+        ],
+    ];
+
     return view('posts', [
-        "title" => "Posts"
+        "title" => "Posts",
+        "posts" => $blog_posts
     ]);
 });
