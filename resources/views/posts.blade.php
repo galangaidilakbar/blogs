c662be565156dcc69019f69c280c499758e98a63
Galangaidil <galangaidil45@gmail.com>
2021-08-02 22:30:10 +0700

    kasih penulis

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.main')
 @section('container')
     @foreach ($posts as $post)
-    <article class="mb-5">
+    <article class="mb-5 border-bottom pb-4">
       <h2>
-        <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
       </h2>
+
+      <p>by Galang aidil akbar in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
       <p>{{ $post->excerpt }}</p>
+
+      <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more</a>
     </article>
     @endforeach
 @endsection
