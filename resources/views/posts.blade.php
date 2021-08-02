e58430213956851515034083eeac8ddf22285f71
Galangaidil <galangaidil45@gmail.com>
2021-08-03 00:13:04 +0700

    N+1 Problem | using eager load instead of lazy load

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.main')
+
 @section('container')
+  <h1 class="mb-5">{{ $title }}</h1>
+
     @foreach ($posts as $post)
     <article class="mb-5 border-bottom pb-4">
       <h2>
--- routes/web.php
+++ routes/web.php
@@ -45,16 +45,15 @@ Route::get('/categories', function () {
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post by Category : $category->name",
+        'posts' => $category->posts->load('category', 'author')
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'title' => 'User Post',
-        'posts' => $author->posts,
+        'title' => "Post By Author : $author->name",
+        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
